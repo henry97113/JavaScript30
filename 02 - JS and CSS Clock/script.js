efe608b6f1d0f5fc8b9d1e894642c1c5d075e6ee
henry97113 <henry97113@gmail.com>
2018-10-09 12:03:03 +0800

    Day 2 - 讓分針時針也會每秒移動

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -2,36 +2,31 @@ const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 const currentTime = new Date();
+const second = currentTime.getSeconds();
+const minute = currentTime.getMinutes();
+const hour = currentTime.getHours();
 
-let secondCount = 0, minuteCount = 0, hourCount = 0;
+let secondCount = 0;
 
 function setClock() {
-    const secondDegrees = ((currentTime.getSeconds()/ 60) * 360) + 90;
-    const minuteDegrees = ((currentTime.getMinutes()/ 60) * 360) + 90;
-    const hourDegrees = ((currentTime.getHours()/ 12) * 360) + 90;
+    const secondDegrees = ((second / 60) * 360) + 90;
+    const minuteDegrees = ((minute / 60) * 360) + ((second / 60) * 6) + 90;
+    const hourDegrees = ((hour / 12) * 360) + ((minute / 60) * 30) + 90;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
     minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 function updateClock() {
-    // 
     secondCount++;
-    const secondDegrees = (currentTime.getSeconds() + secondCount) * 6 + 90;
+    const newSecond = currentTime.getSeconds() + secondCount;
+    const secondDegrees = (newSecond) * 6 + 90;
     let minuteDegrees, hourDegrees;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
-    // 分針進位
-    if((secondDegrees - 90) % 360 === 0) {
-        minuteCount++;
-        minuteDegrees = (currentTime.getMinutes() + minuteCount) * 6 + 90;
-        minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
-    }
-    // 時針進位
-    if ((secondDegrees - 90) % 360 === 0 && (minuteDegrees - 90) % 360 === 0) {
-        hourCount++;
-        hourDegrees = (currentTime.getHours() + hourCount) * 30 + 90;
-        hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-    }
+    minuteDegrees = ((minute / 60) * 360) + ((newSecond / 60) * 6) + 90;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    hourDegrees = ((hour / 12) * 360) + (minute / 60 * 30) + ((newSecond / 3600) * 30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setClock();
